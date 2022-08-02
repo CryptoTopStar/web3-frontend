1dacfeccddbb1bcc207e3054cf73e5f1d853052a
CryptoTopStar <108870471+CryptoTopStar@users.noreply.github.com>
2022-08-02 09:26:45 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 import Web3 from 'web3';
 import {contractAbi, contractAddress} from './utils/constants';
+import React from 'react';
 
-const web3 = new Web3("ws://localhost:8545");
+const web3 = new Web3("ws://localhost:8545")
 const tokenContract = new web3.eth.Contract(contractAbi, contractAddress);
 
 function App() {
-
-  console.log(tokenContract);
- // const balanceOfMyToken = tokenContract.methods.balanceOf().call();
- // console.log(balanceOfMyToken);
+  const [accountAddress, setAccountAddress] = React.useState('');
+  const [showBalance, setShowBalance] = React.useState('');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const balanceOfMyToken = await tokenContract.methods.balanceOf(accountAddress).call();
+    setShowBalance(balanceOfMyToken);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value = {accountAddress}
+          onChange = {(e) => setAccountAddress(e.target.value)}
+          placeholder='account address'
+        />
+        <br/>
+        <button>
+          Show balance
+        </button>
+      </form>
+      <br/>
+      <label>{showBalance}</label>
     </div>
   );
 }
